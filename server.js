10a4cfe96c581428a126057426defdd5642966e2
Naveenprabbhu1308 <naveen11120c@gmail.com>
2026-03-10 11:47:43 +0530

    fix cors

--- server.js
+++ server.js
@@ -10,14 +10,13 @@ app.use(helmet());
 
 app.use(cors({
   origin: [
-    'https://examschedulerapp-eta.vercel.app',
+    process.env.FRONTEND_URL,
     'http://localhost:3000'
   ],
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type','Authorization'],
   credentials: true
 }));
-
 app.use(express.json());
 
 // Routes
